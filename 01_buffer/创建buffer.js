dfd896f76a82aaa8d90e8e186bd6574214c5832c
陈正清MacPro <tenchenzhengqing@qq.com>
2023-07-18 22:01:31 +0800

    代码提交 笔记本接力

---
+++ 01_buffer/创建buffer.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: czqczqzzzzzz(czq)
+ * @Email: tenchenzhengqing@qq.com
+ * @Date: 2023-07-13 15:22:21
+ * @LastEditors: 陈正清MacPro
+ * @LastEditTime: 2023-07-13 15:34:53
+ * @FilePath: /atguigu-nodejs/01_buffer/创建buffer.js
+ * @Description: 创建buffer的方法
+ * 
+ * Copyright (c) by czqczqzzzzzz(czq), All Rights Reserved.
+ */
+
+// 1. alloc
+let buf = Buffer.alloc(10)
+console.log(buf);
+
+// 2. allocUnsafe
